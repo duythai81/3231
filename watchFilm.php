09ff059baacc3870ff978dafcb96bac704b6f936
duythai81 <124331989+duythai81@users.noreply.github.com>
2024-07-10 21:37:20 +0700

    Add files via upload

---
+++ watchFilm.php
@@ -0,0 +1,314 @@
+<?php
+$string =  '<!DOCTYPE html>'.
+// '<html lang="en">'.
+// '<head>'.
+// '    <meta charset="UTF-8">'.
+// '    <meta http-equiv="X-UA-Compatible" content="IE=edge">'.
+// '    <meta name="viewport" content="width=device-width, initial-scale=1.0">'.
+// '    <title>Đăng ký</title>'.
+// '    <link rel="stylesheet" href="./assets/css/register.css">'.
+// '</head>'.
+// '<body>'.
+// '    <div class="login-box">'.
+// '        <div class="logo-header">'.
+// '            <img src="./assets/images/motphimtv.png" alt="">'.
+// '        </div>'.
+// '        <h2>Đăng ký</h2>'.
+// '        <form id="box-main">'.
+// '          <div class="user-box">'.
+// '            <input type="text" name="" required="" id="user-name">'.
+// '            <label>Tên đăng nhập</label>'.
+// '          </div>'.
+// '          <div class="user-box">'.
+// '            <input type="password" name="" required="" id="pass">'.
+// '            <label>Mật khẩu</label>'.
+// '          </div>'.
+// '          <div class="user-box">'.
+// '            <input type="password" name="" required="" id="end-pass">'.
+// '            <label>Nhập lại mật khẩu</label>'.
+// '          </div>'.
+// '          <a href="#" id="click-register">'.
+// '            <span></span>'.
+// '            <span></span>'.
+// '            <span></span>'.
+// '            <span></span>   '.
+// '            <button > Đăng ký</button>'.
+// '            '.
+// '          </a><!DOCTYPE html>'.
+'<html lang="en">'.
+''.
+'<head>'.
+'    <meta charset="UTF-8">'.
+'    <meta http-equiv="X-UA-Compatible" content="IE=edge">'.
+'    <meta name="viewport" content="width=device-width, initial-scale=1.0">'.
+'    <title>Phim hay</title>'.
+'    <link rel="stylesheet" href="./assets/css/style.css"> <!-- link css -->'.
+'    <link rel="stylesheet" href="./assets/css/watchfilm.css"> <!-- link css -->'.
+'    <link rel=\'stylesheet prefetch\' href=\'https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css\'>'.
+'    <link rel="stylesheet" href="./assets/css/watchcomment.css">'.
+'    <link rel=\'stylesheet prefetch\' href=\'https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css\'>'.
+'    '.
+'    <style>'.
+'        .carousel-item .active{'.
+'            display: flex !important;'.
+'        }'.
+'    </style>'.
+'    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>'.
+'    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">'.
+''.
+''.
+'    '.
+'</head>'.
+''.
+'<body>'.
+'   '.
+'    <div class="width100">'.
+'        <div id="fb-root"></div>'.
+'<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v16.0" nonce="XpsqONGt"></script>'.
+'        <div class="container">'.
+'            <div id="header">'.
+'                <div class="logo-header">'.
+'                    <img src="./assets/images/motphimtv.png" alt="">'.
+'                </div>'.
+'                <div class="content-header">'.
+'                    <input type="text" placeholder="Nhập tên bộ phim mà bạn đang muốn tìm kiếm">'.
+'                    <a href=""><i class="fas fa-search"></i></a>'.
+'                </div>'.
+'                <div class="action-header">'.
+'                    <div class="login-user" id="login">'.
+'                        <a href="./login.php"><i class="fa-solid fa-right-to-bracket"></i>Đăng nhập</a>'.
+'                    </div>'.
+'                    <div class="register-user" id="register">'.
+'                        <a href="./register.php"><i class="fa-solid fa-registered"></i>Đăng kí</a>'.
+'                    </div>'.
+'                    <div class="bookmark-user">'.
+'                        <a href="#"><i class="fa-solid fa-bookmark"></i>Bookmark</a>'.
+'                    </div>'.
+'                </div>'.
+'            </div>'.
+'            <div id="menu">'.
+'                <ul class="menu-header">'.
+'                    <li>'.
+'                        <a href="./index.php">Trang chủ</a>'.
+'                    </li>'.
+'                    <li>'.
+'                        <a href="#">Thể loại</a>'.
+'                        <ul class="sub-menu">'.
+'                            <li>'.
+'                                <a href="#">Ngôn tình</a>'.
+'                            </li>'.
+'                            <li>'.
+'                                <a href="#">Ba lát</a>'.
+'                            </li>'.
+'                            <li>'.
+'                                <a href="#">Bay bổng</a>'.
+'                            </li>'.
+'                            <li>'.
+'                        </ul>'.
+'                    </li>'.
+'                    <li>'.
+'                        <a href="#">Quốc gia</a>'.
+''.
+'                    </li>'.
+'                    <li>'.
+'                        <a href="#">Phim mới</a>'.
+'                    </li>'.
+'                    <li>'.
+'                        <a href="#">Hot Hit</a>'.
+'                    </li>'.
+'                    <li>'.
+'                        <a href="#">Thịnh hành</a>'.
+'                    </li>'.
+'                    <li>'.
+'                        <a href="#">TV nhạc</a>'.
+'                    </li>'.
+'                </ul>'.
+'            </div>'.
+'        </div>'.
+'    </div>'.
+'    <div class="content">'.
+'        <div class="headerr">'.
+'            <h1 style="color: white; justify-content: center; text-align: center;">'.
+'                         Chúc quý vị xem phim vui vẻ ! '.
+'            </h1>'.
+'        </div>'.
+'        <div class="video">'.
+'        <iframe width="1200" height="700" src="https://www.youtube.com/embed/LTV8Nj-V0Kc"  frameborder="0"></iframe> <br>'.
+'        <div class="stars">'.
+'            <form action="">'.
+'              <input class="star star-5" id="star-5" type="radio" name="star"/>'.
+'              <label class="star star-5" for="star-5"></label>'.
+'              <input class="star star-4" id="star-4" type="radio" name="star"/>'.
+'              <label class="star star-4" for="star-4"></label>'.
+'              <input class="star star-3" id="star-3" type="radio" name="star"/>'.
+'              <label class="star star-3" for="star-3"></label>'.
+'              <input class="star star-2" id="star-2" type="radio" name="star"/>'.
+'              <label class="star star-2" for="star-2"></label>'.
+'              <input class="star star-1" id="star-1" type="radio" name="star"/>'.
+'              <label class="star star-1" for="star-1"></label>'.
+'            </form>'.
+'          </div>'.
+'        </div>'.
+'        <div class="container_film_khac">'.
+'            <h5>Có thể bạn muốn xem</h5>'.
+'            <div class="film_khac">'.
+'                <div id="carouselExample" class="carousel slide">'.
+'                    <div class="carousel-inner">'.
+'                      <div class="carousel-item active">'.
+'                       <img height="200px" width="180px " style="margin-right:10px; margin-left: 15px;" src="https://upload.wikimedia.org/wikipedia/vi/0/0e/Little_Star_Wars_2021.jpg" alt="">'.
+'                       <img height="200px" width="180px  "  style="margin-right:15px" src="https://ngaymoionline-cdn.mastercms.vn/stores/news_dataimages/dangthuthuy/122022/14/10/2234_avatar-1-3796.jpg?rt=20221214102314" alt="">'.
+'                       <img height="200px" width="180px  "  style="margin-right:15px"src="https://image.mpapis.xyz/poster/an-danh-phan-1-11002.jpg" alt="">'.
+'                       <img height="200px" width="180px  "  style="margin-right:15px"src="https://blogradio.vn/uploads/cms/source/2021/20/moitinhdaucuatoi_(1).jpg" alt="">'.
+'                       <img height="200px" width="180px  " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9zSadxGzPiewj6NxUbPSId43EJJrfqJzYrWnZnQ2VvrBlhaRJ4ftM-ANVsIQzWA_56AY&usqp=CAU" alt="">'.
+'                      </div>'.
+'                      <div class="carousel-item">'.
+'                       <img height="200px" width="180px  " style="margin-right:10px; margin-left: 15px" src="https://2sao.vietnamnetjsc.vn/images/2022/10/25/11/15/black-adam-1.jpg" alt="">'.
+'                       <img height="200px" width="180px  " style="margin-right:15px" src="https://static2.vieon.vn/vieplay-image/thumbnail_v4/2023/03/23/8mkmn3t9_1920x1080-tinhyeumathoi1.png" alt="">'.
+'                       <img height="200px" width="180px  " style="margin-right:15px"  src="https://ghienphim3.net/uploads/sat-thu-john-wick-phan-4-john-wick-chapter-4_banner_.jpg?v=1680153965" alt="">'.
+'                       <img height="200px" width="180px  " style="margin-right:15px"  src="https://newsmd2fr.keeng.net/tiin/archive/imageslead/2022/09/04/90_84362b80d4e0d57d2cf082535ba202e2.jpg" alt="">'.
+'                       <img height="200px" width="180px  " src="https://static2.vieon.vn/vieplay-image/poster_v4/2022/01/11/1743wj7o_660x946-tanthandieudaihiep.jpg" alt="">'.
+'                      </div>'.
+'                      <div class="carousel-item">'.
+'                       <img height="200px" width="180px  " style="margin-right:10px; margin-left: 15px;" src="https://img.cdn.vncdn.io/cinema/img/1769222641137689-5.jpg" alt="">'.
+'                        <img height="200px" width="180px  "   style="margin-right:15px" src="https://image.mpapis.xyz/poster/chi-chi-em-em-2-11092.jpg" >'.
+'                        <img height="200px" width="180px  " style="margin-right:15px"  src="https://subnhanh.cdn1-img.net/toi-la-chihiro-call-me-chihiro-2023-vietsub_1.jpg" alt="">'.
+'                        <img height="200px" width="180px  " style="margin-right:15px" src="https://gamek.mediacdn.vn/133514250583805952/2022/4/4/fc2-164904738916349427555.jpg" alt="">'.
+'                        <img height="200px" width="180px  "  src="https://rapchieuphim.com/photos/3/phim-ngoi-den-ky-quai/phim-ngoi-den-ky-quai.jpg" alt="">'.
+'                      </div>'.
+'                    </div>'.
+'                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">'.
+'                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>'.
+'                      <span class="visually-hidden">Previous</span>'.
+'                    </button>'.
+'                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">'.
+'                      <span class="carousel-control-next-icon" aria-hidden="true"></span>'.
+'                      <span class="visually-hidden">Next</span>'.
+'                    </button>'.
+'                  </div>'.
+'            </div>'.
+'    </div>'.
+'            '.
+'        '.
+'          '.
+'         <!-- <div class="comment">'.
+'            <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="100%" data-numposts="10"></div>'.
+'    </div>'.
+'    </div> -->'.
+''.
+''.
+''.
+''.
+''.
+'    <!-- comment -->'.
+'    <div class="container-form-comment">'.
+'        <div class="comment-head">'.
+'            <div><span id="count">0</span> bình luận</div>'.
+'            <div class="sort-comment">'.
+'                <!-- <span>Sắp sếp theo</span> -->'.
+'                <!-- <select name="" id="">'.
+'                        <option value="newest">Mới nhất</option>'.
+'                        <option value="oldest">Cũ nhất</option>'.
+'                </select> -->'.
+'            </div>'.
+'        </div>'.
+''.
+''.
+''.
+''.
+''.
+''.
+''.
+'        <!-- comment -->'.
+'        <div class="container-comment-user">'.
+'            <div class="action-comment-user">'.
+'                <div class="left-action-comment-user">'.
+'                    <img src="https://inkythuatso.com/uploads/thumbnails/800/2023/03/9-anh-dai-dien-trang-inkythuatso-03-15-27-03.jpg" alt="">'.
+'                </div>'.
+'                <div class="right-action-comment-user">'.
+'                    <div>'.
+'                    <!-- <form> -->'.
+'                        <textarea id="comment" placeholder="Nhập bình luận..." value="" style="height: 5vw;"></textarea>'.
+'                    <!-- </form> -->'.
+'                </div>'.
+'                <div class="comment-btn">'.
+'                    <input id="submit" type="submit" value="Đăng">'.
+'                </div>'.
+'            </div>        '.
+'        </div>'.
+'        <div>'.
+'            <div class="show-comment-user">'.
+'                <h4>Bình luận</h4>'.
+'                <div id="comment-box"></div>'.
+'            </div>'.
+'            <!-- <div class="post-comment">'.
+'                <div class="list">'.
+'                    <div class="comment-avt">'.
+'                        <div class="user-image"><img src="https://inkythuatso.com/uploads/thumbnails/800/2023/03/9-anh-dai-dien-trang-inkythuatso-03-15-27-03.jpg" alt=""></div>'.
+'                    </div>'.
+'                    <div class="user">'.
+'                        <div class="user-meta">'.
+'                            <div class="comment-info">'.
+'                                <div class="name">qqqsfsadgsgasdg</div>'.
+'                                <div class="day">10 days ago</div>'.
+'                            </div>'.
+'                            <div class="info-btn">'.
+'                            </div>'.
+'                        </div>'.
+'                        <div class="comment-post">test comment</div>'.
+'                    </div>'.
+'                    '.
+'                </div>'.
+'            </div> -->'.
+'        </div>'.
+'    </div>'.
+''.
+'</div>'.
+''.
+'</div>'.
+''.
+'</div>'.
+''.
+'    <div class="width100">'.
+'            '.
+'        <div class="container">'.
+'        '.
+'            <div id="footer">'.
+'                    <div class="left-footer">'.
+'                <p>'.
+'                    <a href="">Xem phim online</a> miễn phí chất lượng cao với phụ đề tiếng việt - thuyết minh - lồng tiếng. Mọt phim có nhiều thể loại phim phong phú, đặc sắc, nhiều bộ phim hay nhất - mới nhất.'.
+''.
+'                    Website motphim.net với giao diện trực quan, thuận tiện, tốc độ tải nhanh, thường xuyên cập nhật các bộ phim mới hứa hẹn sẽ đem lại những trải nghiệm tốt cho người dùng.'.
+'                </p>'.
+'            </div>'.
+'            <div class="right-footer">'.
+'                <div class="column">'.
+'                    <a href="">Quy định</a>'.
+'                    <a href="">Điều khoản chung</a>'.
+'                    <a href="">Chính sách riêng tư</a>'.
+'                </div>'.
+''.
+'                <div class="column">'.
+'                    <a href="">Giới thiệu</a>'.
+'                    <a href="">Trang chủ</a>'.
+'                    <a href="">Facebook</a>'.
+'                </div>'.
+'            </div>'.
+'            </div>'.
+'            '.
+'        </div>'.
+'    </div>'.
+'    <script src="./assets/js/dataFilm.js"></script>'.
+'    <script src="./assets/js/comment.js"></script>'.
+'    <script src="./assets/js/showdataforWatchfilm.js"></script>'.
+'</body>'.
+''.
+'</html>'.
+'        </form>'.
+'      </div>'.
+''.
+'      <script src="./assets/js/login.js"></script>'.
+'</body>'.
+'</html>';
+
+ echo $string; 
+?>
